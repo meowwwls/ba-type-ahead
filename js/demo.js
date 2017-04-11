8bbf46f08324b37653f635a5fc05f0002708beda
meowwwls <meowwwls@users.noreply.github.com>
2017-04-11 17:03:45 -0500

    Fix Safari input issue/change styles

--- js/demo.js
+++ js/demo.js
@@ -1,147 +1,136 @@
-(function typeAheadBADemo() {
+const students = [
+  { name: 'Lena Faure', url: 'https://students.bovacademy.com/'},
+  { name: 'Melissa Miller', url: 'https://students.bovacademy.com/'},
+  { name: 'Rochell Langam', url: 'https://students.bovacademy.com/'}
+];
+
+var articles = [
+  {
+    title: 'Always Be Closure(ing)',
+    url: 'https://blog.bovacademy.com/',
+  },
+  {
+    title: 'Getting Closer to an Inclusive Web',
+    url: 'https://blog.bovacademy.com/'
+  },
+  {
+    title: 'Git Clone Explained',
+    url: 'https://blog.bovacademy.com/'
+  },
+  {
+    title: 'Looking closer at closures',
+    url: 'https://blog.bovacademy.com/'
+  },
+  {
+    title: 'Margin Mysteries Explained',
+    url: 'https://blog.bovacademy.com/'
+  }
+];
+
+function typeAhead(data, options) {
+  // helper functions
+  function getKeyCode(e) {
+    const keyCode = e.key || e.which || e.keyCode;
+    return keyCode;
+
+  }
+
   const input = document.getElementById('search-field');
   const results = document.getElementById('search-results');
 
-  // filler data; would pull from JSON
-  const students = [
-    {
-      author: {
-        name: 'Lena Faure',
-        url: 'https://students.bovacademy.com/'
-      },
-      articles: [
-        {
-          title: 'Always Be Closure(ing)',
-          url: 'https://blog.bovacademy.com/'
-        }
-      ]
-    },
-    {
-      author: {
-        name: 'Melissa Miller',
-        url: 'https://students.bovacademy.com/'
-      },
-      articles: [
-        {
-          title: 'Getting Closer to an Inclusive Web',
-          url: 'https://blog.bovacademy.com/'
-        },
-        {
-          title: 'Git Clone Explained',
-          url: 'https://blog.bovacademy.com/'
-        },
-      ]
-    },
-    {
-      author: {
-        name: 'Rochell Langam',
-        url: 'https://students.bovacademy.com/'
-      },
-      articles: [
-        {
-          title: 'Looking closer at closures',
-          url: 'https://blog.bovacademy.com/'
-        },
-        {
-          title: 'Margin Mysteries Explained',
-          url: 'https://blog.bovacademy.com/'
-        },
-      ]
-    },
-  ];
-
   // flag used for whether or not to hide the results when body is clicked or esc is pressed
   let resultsShown = false;
+  form = document.querySelector('form');
 
-  function findMatch(input, students) {
+
+  function findMatch(input) {
     const escaped = input.replace(/\./g, '\\.');
     const regex = new RegExp(escaped, 'gi');
     const matches = [];
 
     if (input.trim()) {
+      // go through each object in the array with our data
+      data.forEach(obj => {
+        // store the array and properties to search from the current option / object for ease access
+        const { array, props } = obj;
 
-      students.forEach(student => {
-        const s = student.author.name.match(regex);
+        // for each item in the object's array (e.g., for each article)
+        array.forEach(item => {
 
-        if (s) {
-          matches.push(student.author);
-        }
+          // test the item's properties against the input (e.g., test each article's title)
+          props.forEach(prop => {
 
-        student.articles.forEach(article => {
-          const a = article.title.match(regex);
-          if (a) {
-            matches.push(article);
-          }
+            // make sure the property exists
+            if (item.hasOwnProperty(prop)) {
+              const matched = item[prop].match(regex);
+
+              if (matched) {
+                matches.push(
+                  {
+                    icon: obj.icon || null,
+                    textToRender: item[prop],
+                    url: item.url || '',
+                  });
+              }
+            }
+          });
         });
-
       });
+
     }
 
     return matches;
   }
 
-  function wrapMatch(type, str, input) {
+  function wrapMatch(iconType, str, input) {
     const escaped = input.replace(/\./g, '\\.');
     const regex = new RegExp(escaped, 'gi');
-    let wrappedMatch = `<svg class="search__icon icon--${type}">
-                          <use xlink:href="#icon-${type}"></use>
-                        </svg>`;
 
-    wrappedMatch += str.replace(regex, `<span class="suggestion__highlight">${input}</span>`);
+    let wrappedMatch = iconType ? `<i class="search__icon suggestion__icon material-icons" aria-hidden="true">${iconType}</i>` : '';
+
+    wrappedMatch += str.replace(regex, match => `<span class="suggestion__highlight">${match}</span>`);
 
     return wrappedMatch;
   }
 
-  function createMatchLI(url, wrappedMatch) {
-    // put -1 tabindex on links so user will not have to tab through all results in order to get out of the list
+  // moreClass is optional; only passed when the LI to be created is for the "see more" item
+  function createMatchLI(url, wrappedMatch, moreClass = '') {
     return `
-      <li class="search__suggestion">
+      <li class="search__suggestion ${moreClass}">
         <a href="${url}" tabindex="-1">${wrappedMatch}</a>
       </li>`;
   }
 
-  function appendMatches(input, matches, el) {
+  function appendMatches(input, matches, el, limit) {
+    el.innerHTML = '';
+    const matchesToAppend = limit ? matches.slice(0, limit) : matches;
     let html = '';
-    const authors = [];
-    const articles = [];
 
-    matches.forEach(match => {
-
-      if (match.hasOwnProperty('name')) {
-        authors.push(match);
-      }
-
-      if (match.hasOwnProperty('title')) {
-        articles.push(match);
-      }
+    matchesToAppend.forEach((match) => {
+      const wrappedMatch = wrapMatch(match.icon, match.textToRender, input);
+      html += createMatchLI(match.url, wrappedMatch);
     });
 
-    authors.sort();
-    articles.sort();
-
-    authors.forEach(author => {
-      const wrappedMatch = wrapMatch('user-circle-o', author.name, input);
-      html += createMatchLI(author.url, wrappedMatch);
-    });
-
-    articles.forEach(article => {
-      const wrappedMatch = wrapMatch('article', article.title, input);
-      html += createMatchLI(article.url, wrappedMatch);
-    });
+    // if there are more results than those being displayed
+    if (matchesToAppend.length < matches.length) {
+      let wrappedMatch = wrapMatch('', 'See more results', '');
+      wrappedMatch += `<i class="search__icon suggestion__icon material-icons" aria-hidden="true">arrow_forward</i>`;
+      html += createMatchLI('#?', wrappedMatch, 'search__suggestion--more');
+    }
 
     el.innerHTML = html;
   }
 
   function handleMatches(e) {
     const trimmedInput = e.target.value.trim().toLowerCase();
-    const matches = findMatch(trimmedInput, students);
+    const matches = findMatch(trimmedInput);
 
     if (results.classList.contains('js-is-hidden')) {
       results.classList.remove('js-is-hidden');
     }
 
     if (matches.length) {
-      appendMatches(trimmedInput, matches, results);
+      appendMatches(trimmedInput, matches, results, (typeof options === 'object' && options.limit));
       resultsShown = true;
     } else {
       closeList();
@@ -151,7 +140,7 @@
 
   // when the input is focused and the down arrow is pressed, send focus to the list of matches
   function focusList() {
-    const hasResults = results.children;
+    const hasResults = results && results.children;
 
     // focus the first link in the first list item if there are matches
     if (hasResults.length) {
@@ -160,34 +149,44 @@
   }
 
   function handleResultFocus(e) {
-    if (e.which === 40) {
+    const keyCode = getKeyCode(e);
+
+    if (keyCode === 'ArrowDown' || keyCode === 40) {
+
       if (e.target === results.lastElementChild.firstElementChild) {
         input.focus();
       } else {
         e.target.parentNode.nextElementSibling.firstElementChild.focus();
       }
-    } else if (e.which === 38) {
+
+    } else if (keyCode === 'ArrowUp' || keyCode === 38) {
+
       if (e.target === results.firstElementChild.firstElementChild) {
         input.focus();
       } else {
         e.target.parentNode.previousElementSibling.firstElementChild.focus();
       }
+    } else if (keyCode === 'Enter' && e.target.parentNode.classList.contains('search__suggestion--more')) {
+      e.preventDefault();
+      form.submit();
     }
+
   }
 
   function closeList() {
-    // input.focus(); // not sure about this; then if someone has tabbed to another region and hits esc, the input will be focused again
     results.classList.add('js-is-hidden');
     resultsShown = false;
   }
 
-
   input.addEventListener('keyup', function(e) {
-    if (e.which === 40) {
+    const keyCode = getKeyCode(e);
+
+    if (keyCode === 'ArrowDown' || keyCode === 40) {
       focusList();
-    } else {
+    } else if ((keyCode !== 'ArrowLeft' && keyCode !== 37) && (keyCode !== 'ArrowRight' && keyCode !== 39)) {
       handleMatches(e);
     }
+
   }, false);
 
   // will allow results to be displayed if a user has previously closed them and then refocused the field
@@ -198,16 +197,40 @@
 
   // handle esc key event to hide results if shown
   document.body.addEventListener('keyup', function(e) {
-    if (e.which === 27 && resultsShown) {
+    const keyCode = getKeyCode(e);
+    if ((keyCode === 'Escape' || keyCode === 27) && resultsShown) {
       closeList();
     }
+
   }, false);
 
-  // hide results if anywhere outside of the results list or input is clicked
   document.body.addEventListener('click', function(e) {
+    // if the results are displayed and somewhere outside of the form area is clicked, hide the results
     if (resultsShown && e.target !== input && e.target.parentNode.parentNode !== results) {
       closeList();
     }
+    // if "see more results" is clicked, submit the search form
+    else if (e.target.parentNode.classList.contains('search__suggestion--more')) {
+      e.preventDefault();
+      form.submit();
+    }
   }, false);
+}
 
-}());
+typeAhead(
+  [
+    {
+      array: students,
+      props: ['name'],
+      icon: 'person'
+    },
+    {
+      array: articles,
+      props: ['title'],
+      icon: 'subject'
+    }
+  ],
+  {
+    limit: 6
+  }
+);
